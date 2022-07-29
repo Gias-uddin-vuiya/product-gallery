38e6ed013986ded7314d40ae65254081c7fa3266
Gias uddin vuiya <giasuddin9071@gmail.com>
2022-07-29 23:05:14 +0600

    product gallery successful virsion 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
-ECHO is on.
+'use strict';
+
+const laptopImage1Elm = document.querySelector('#laptop-1');
+const laptopImage2Elm = document.querySelector('#laptop-2');
+const laptopImage3Elm = document.querySelector('#laptop-3');
+const laptopImage4Elm = document.querySelector('#laptop-4');
+const laptopImage5Elm = document.querySelector('#laptop-5');
+
+const imageGallery = document.querySelector('.main-image img');
+
+
+laptopImage1Elm.addEventListener('click',(evt) => 
+{
+	imageGallery.src = '';
+	imageGallery.src = 'image/image-number1.jpg'
+});
+
+laptopImage2Elm.addEventListener('click',(evt) => 
+{
+	imageGallery.src = '';
+	imageGallery.src = 'image/image-number2.jpg';
+});
+
+laptopImage3Elm.addEventListener('click',(evt) => 
+{
+	imageGallery.src = '';
+	imageGallery.src = 'image/image-number3.jpg';
+});
+
+laptopImage4Elm.addEventListener('click',(evt) => 
+{
+	imageGallery.src = '';
+	imageGallery.src = 'image/image-number4.jpg';
+});
+
+laptopImage5Elm.addEventListener('click',(evt) => 
+{
+	imageGallery.src = '';
+	imageGallery.src = 'image/image-number5.jpg';
+})
+
